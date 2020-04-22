17716549ab4fe653d64c96a4bcd82a85eb4a007b
AngelGarBar <56541642+AngelGarBar@users.noreply.github.com>
2020-04-22 14:46:48 +0200

    Script movimiento, gitignore modificado
    Se ha generado el script para el movimiento del jugador y se ha modificado el gitignore para evitar archivos muy grandes

---
+++ Juego2DCurso/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	// Se programa el movimiento en las distintas teclas
+	void Update () {
+		if(Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Translate(new Vector3(-0.1f,0.0f));
+		}
+
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Translate(new Vector3(0.1f, 0.0f));
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow)|| Input.GetKey(KeyCode.W))
+		{
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f,60.0f));
+		}
+	}
+}
