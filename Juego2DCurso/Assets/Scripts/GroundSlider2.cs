e98a4d946a528debf436b67511a4cfbf82558943
AngelGarBar <56541642+AngelGarBar@users.noreply.github.com>
2020-04-30 14:49:07 +0200

    Script final movimiento suelo, sustitucion assets temporales
    Se ha generado el script final para dar al suelo la sensación de movimiento.
    Se han sustituido todos los assets de pruebas por versiones finales.

---
+++ Juego2DCurso/Assets/Scripts/GroundSlider2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSlider2 : MonoBehaviour {
+
+	public float scrollSpeed;
+	public Renderer rend;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Mueve el offset del material del suelo, creando el efecto de movimiento del suelo
+	void Update () {
+		float offset = Time.time * scrollSpeed;
+		rend.material.SetTextureOffset("_MainTex",new Vector2(offset, 0.0f));
+	}
+}
