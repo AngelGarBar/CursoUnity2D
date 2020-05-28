45a885ab15a91cd7f300bb93d7fcb5ca35d74518
AngelGarBar <56541642+AngelGarBar@users.noreply.github.com>
2020-05-28 15:47:55 +0200

    Interfaz, Menu y audio implementados

--- Juego2DCurso/Assets/Scripts/GameManager.cs
+++ Juego2DCurso/Assets/Scripts/GameManager.cs
@@ -1,22 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public Text vidas_txt;
 	public Text monedas_txt;
+	public Text record_txt;
+	public Text score_txt;
+
+	public GameObject gameOverPanel;
 
 	public Player pc;
 	// Use this for initialization
 	void Start () {
-		
+		gameOverPanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		vidas_txt.text = pc.Life.ToString();
 		monedas_txt.text = pc.Coins.ToString();
+
+		if(pc.Life <= 0 && gameOverPanel.activeSelf == false)
+		{
+			gameOverPanel.SetActive(true);
+			record_txt.text = PlayerPrefs.GetInt("Coins").ToString();
+			score_txt.text = pc.Coins.ToString();
+		}
+	}
+
+	public void resetGame()
+	{
+		SceneManager.LoadScene("Main");
 	}
 }
